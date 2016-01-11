b809bb12bd58c8d3b6c87221c48115ab6ef9f94e
Joe <joeshimwell@gmail.com>
2016-01-11 21:05:57 +0000

    Fourth commit

--- PlantTweet.py
+++ PlantTweet.py
@@ -6,9 +6,11 @@ from twython import Twython, TwythonError
 
 camera = picamera.PiCamera()
 
+filename = ""
+message = ""
 
 def image_Tweet(status_update):	#this function uploads a photo to twitter
-        photo = open('image.jpg', 'rb')
+        photo = open(message, 'rb')
         response = twitter.upload_media(media = photo)
         try:
             twitter.update_status(status = status_update, media_ids=[response['media_id']])
@@ -16,8 +18,9 @@ def image_Tweet(status_update):	#this function uploads a photo to twitter
             print e
 
 def snap_Photo():	#this function takes a photograph
-	camera.capture('image.jpg')
-	
+	camera.capture(message)
+	time.sleep(4)
+
 #load the config file and create the API object
 config = {}
 execfile("PlantTweet_conf.py", config)
@@ -25,19 +28,21 @@ twitter = Twython(config["app_key"],config["app_secret"],config["oauth_token"],c
 
 Loop = True
 
+# safe code below
 while Loop == True:
 	hour = int(time.strftime("%H"))
 	minute = int(time.strftime("%M"))
 	second = int(time.strftime("%S"))	
+	message = (time.strftime("%d-%m-%y %H:%M:%S ") + "plant.jpg")		
 	
-	if hour % 1 == 0 and minute == 0 and second == 0:
+	if hour % 1 == 0 and minute % 1 == 0 and second % 20 == 0:
 		snap_Photo()
-		message = "The %d o'clock image" % hour
 		image_Tweet(message)
-	
+		print "Tweeted: ",				
+		print message
+			
+	#delete the file
+		time.sleep(3)
+		subprocess.check_call(["sudo","rm", message])	
+		print "Image deleted"
 	#time.sleep(1)
-#	Loop = False
-
-
-#snap_Photo()
-#image_Tweet()

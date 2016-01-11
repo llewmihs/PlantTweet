21c628b088845ee94a01b48fdb9d6146effdd18a
Joe <joe.shimwell@unn.ac.uk>
2016-01-11 14:35:43 +0000

    First commit

---
+++ PlantTweet.py
@@ -0,0 +1,18 @@
+#subprocess allows python to run commands in the terminal - 
+#this is for the DropBox upload
+import subprocess
+
+#the picamera to take the snaps!
+import picamera
+
+#time will let us take photos at different times of the day
+import time
+
+#twython allows us to upload images to twitter
+from twython import Twython
+
+#load the config file
+config = {}
+execfile("PlatTweet_conf.py", config)
+
+#create the twitter API object

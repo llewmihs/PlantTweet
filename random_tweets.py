be31b97fac3fffe60f8221bfebab518f58a7ddfa
llewmihs <joeshimwell@gmail.com>
2016-01-12 13:22:36 +0000

    Create random_tweets.py

---
+++ random_tweets.py
@@ -0,0 +1,11 @@
+tweetList = [
+	"Two in the hand is worth one in the bush",
+	"A watched pot never boils",
+	"Destroy the seed of evil, or it will grow up to your ruin.",
+	"A small fire is soon quenched.",
+	"The Changjiang River waves behind drive the waves ahead.",
+	"Trial often exhibits truly wonderful results.",
+	"When baffled in one direction a man of energy will not despair, but will find another way to his object.",
+	"A good opportunity is seldom presented, and is easily lost.",
+	"Father's debt, son to give back."
+	]
